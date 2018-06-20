22720333838213019eaa0c1eb8df3e87dc253091
Zach Schuermann <zachary.zvs@gmail.com>
2018-06-20 18:12:13 -0400

    deployed MVP

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import scraper
+import logging
 app = Flask(__name__)
 
 @app.route('/')
@@ -15,5 +16,13 @@ def enter_name():
     max_ = max(values)
     return render_template('chart.html', values=values, labels=labels, max=max_)
 
+@app.errorhandler(500)
+def server_error(e):
+    logging.exception('An error occurred during a request.')
+    return """
+    An internal error occurred: <pre>{}</pre>
+    See logs for full stacktrace.
+    """.format(e), 500
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host='127.0.0.1', port=8080, debug=True)
